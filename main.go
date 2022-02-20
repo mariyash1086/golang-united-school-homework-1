3fc115d7420aa32597e4d55dc8cfb777cde4bd36
Mariyash1086 <milly10031986@gmail.com>
2022-02-20 12:34:33 +0400

    its HW1, attempt 2

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package solution
 
 import (
-	"fmt"
 	"github.com/kyokomi/emoji/v2"
 )
 
-func main() {
+func main() string {
 
-	helloMessage := emoji.Sprint("Hello :world_map:!")
-	fmt.Println(helloMessage)
+	return emoji.Sprint("Hello :world_map:!")
 
 }
