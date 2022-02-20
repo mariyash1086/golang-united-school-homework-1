63541658c69304515c0b753babd5d3a59e84c920
Mariyash1086 <milly10031986@gmail.com>
2022-02-20 12:20:26 +0400

    its HW1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mariyash1086/golang-united-school-homework-1
+
+go 1.17
+
+require github.com/kyokomi/emoji/v2 v2.2.8 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package golang_united_school_homework_1
+
+import (
+	"fmt"
+	"github.com/kyokomi/emoji/v2"
+)
+
+func main() {
+
+	helloMessage := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(helloMessage)
+
+}
